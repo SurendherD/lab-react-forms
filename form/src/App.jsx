76b0fd6b9c49f1dea72c8dbd3596ac7f74cb1265
SurendherReddy <surendher4423h@gmail.com>
2024-06-05 21:49:47 +0530

    completed

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import {useFormik} from 'formik'
+import React, { useState } from 'react';
+import {useFormik,Form} from 'formik';
+import './App.css'
 
 function App() {
+  
+  const [submitted, setSubmit] = useState(false);
+  const [validate, setValidate] = useState(false);
 
   const formik=useFormik({
     initialValues: {
@@ -11,8 +15,14 @@ function App() {
       contact:''
     },
 
-    handleSubmit:(values)=>{
-        console.log(values);
+    validateOnChange:false,
+
+    onSubmit:(values)=>{
+        if(values.firstname && values.lastname && values.mail && values.contact){
+          setValidate(true);
+          setSubmit(true)
+        }
+
     },
 
     validate:(values) =>{
@@ -29,31 +39,35 @@ function App() {
       if(!values.contact){
         errors.contact = "Phone number Rquired"
       }
-      return errors;
-    }
+      
+    return errors;
+  }
+
   })
 
+
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <label>First Name : </label>
-        <input type="text" name='firstname' id='firstname' value={formik.values.firstname} onChange={formik.handleChange}/> <br />
-        {formik.errors.firstname?<div className="errors">{formik.errors.firstname}</div>:null}
+    <div  className='container'>
+      <div className='innerContainer'>
+        <form onSubmit={formik.handleSubmit} autoComplete='off'>
+        {submitted && validate?<div className="success">Registration succesfull</div>:<div className="success">Registration Form</div>}
+          <input type="text" name='firstname' id='firstname' placeholder='firstname' value={formik.values.firstname} onChange={formik.handleChange}/> <br />
+          {formik.errors.firstname?<div className="errors">{formik.errors.firstname}</div>:null}
 
-        <label>Last Name : </label>
-        <input type="text" name='lastname' id='lastname' value={formik.values.lastname} onChange={formik.handleChange}/> <br />
-        {formik.errors.lastname?<div className="errors">{formik.errors.lastname}</div>:null}
+          <input type="text" name='lastname' id='lastname' placeholder='lastname' value={formik.values.lastname} onChange={formik.handleChange}/> <br />
+          {formik.errors.lastname?<div className="errors">{formik.errors.lastname}</div>:null}
 
-        <label>E-mail : </label>
-        <input type="email" name='mail' id='mail' value={formik.values.mail} onChange={formik.handleChange}/> <br />
-        {formik.errors.mail?<div className="errors">{formik.errors.mail}</div>:null}
+        
+          <input type="email" name='mail' id='mail' placeholder='E-mail' value={formik.values.mail} onChange={formik.handleChange}/> <br />
+          {formik.errors.mail?<div className="errors">{formik.errors.mail}</div>:null}
 
-        <label>Contact : </label>
-        <input type='tel' name='contact' id='contact' value={formik.values.contact} onChange={formik.handleChange}/> <br />
-        {formik.errors.contact?<div className="errors">{formik.errors.contact}</div>:null}
+        
+          <input type='tel' name='contact' id='contact' placeholder='Mobile Number' value={formik.values.contact} onChange={formik.handleChange}/> <br />
+          {formik.errors.contact?<div className="errors">{formik.errors.contact}</div>:null}
 
-        <button type='submit' >Register</button>
-      </form>
+          <button type='submit'>Register</button>
+        </form>
+      </div>
     </div>
   )
 }
